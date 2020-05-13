eca1ae840aa445910c158a75a5d90e87d2c1f0b0
Sama552 <sama.naraghi@gmail.com>
2020-05-13 22:23:21 +0200

    argeggio wroks but also doesnt

--- Prac5/Sama's Code/top.v
+++ Prac5/Sama's Code/top.v
@@ -5,6 +5,7 @@ module top(
     // Check your constraint file to get the right names
     input  CLK100MHZ,
     input [7:0] SW,
+    input BTNL,
     output AUD_PWM, 
     output AUD_SD,
     output [2:0] LED
@@ -12,6 +13,7 @@ module top(
     
     // Toggle arpeggiator enabled/disabled
     wire arp_switch;
+    reg arp_state = 0;
     Debounce change_state (CLK100MHZ, BTNL, arp_switch); // ensure your button choice is correct
     
     // Memory IO
@@ -33,8 +35,6 @@ module top(
     );    
     //PWM Out - this gets tied to the BRAM
     reg [10:0] PWM;
-    wire  PWMout;
-    reg  g;
     
     // Instantiate the PWM module
     pwm_module PWM1 (CLK100MHZ, PWM, AUD_PWM);
@@ -47,20 +47,64 @@ module top(
     // keep track of variables for implementation
     reg [26:0] note_switch = 0;
     reg [1:0] note = 0;
-    reg [8:0] f_base = 0;
+    reg [11:0] f_base = 0;
+    
+    always @(posedge arp_switch) begin
+        arp_state = !arp_state;
+    end
     
 always @(posedge CLK100MHZ) begin   
-     PWM <= douta; // tie memory output to the PWM input
-    f_base[8:0] = 746 + SW[7:0]; // get the "base" frequency to work from 
+    PWM <= douta; // tie memory output to the PWM input
+    f_base = 746 + SW[7:0]; // get the "base" frequency to work from 
     
-    // Loop to change the output note IF we're in the arp state
-    if (clkdiv == 1493) begin
-        addra <= addra + 1'b1;
-        clkdiv <= 0;
-    end else clkdiv <= clkdiv + 1'b1;
-
-    // FSM to switch between notes, otherwise just output the base note.
+    clkdiv <= clkdiv + 1'b1;
     
+    if (arp_state) begin
+        // FSM to switch between notes, otherwise just output the base note.
+        note_switch = note_switch + 1;
+        if (note_switch == 50000000) begin
+            note = note +1;
+            note_switch = 0;
+        end
+        case (note)
+            0: begin
+                if (clkdiv >= f_base*2) begin
+                    clkdiv[12:0] <= 0;
+                    addra <= addra + 1;
+                end
+            end
+            1: begin
+                if (clkdiv >= f_base*5/4) begin
+                    clkdiv[12:0] <= 0;
+                    addra <= addra + 1;
+                end
+            end
+            2: begin
+                if (clkdiv >= f_base*3/2) begin
+                    clkdiv[12:0] <= 0;
+                    addra <= addra + 1;
+                end
+            end
+            3: begin
+                if (clkdiv >= f_base) begin
+                    clkdiv[12:0] <= 0;
+                    addra <= addra + 1;
+                end
+            end
+            default: begin
+                if (clkdiv >= 1493) begin
+                    clkdiv[12:0] <= 0;
+                    addra <= addra + 1;
+                end
+            end
+        endcase;
+    end 
+    else begin
+        if (clkdiv >= 1493) begin
+            addra <= addra + 1'b1;
+            clkdiv <= 0;
+        end
+    end
 end
 
 
